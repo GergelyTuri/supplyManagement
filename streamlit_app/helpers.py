752715ae7ab129c2acd3b4161eb0b3745a22e707
GergelyTuri <gt2253@columbia.edu>
2024-01-14 22:17:37 -0500

    Update .gitignore and use st.secrets.get() for BASE_URL

--- streamlit_app/helpers.py
+++ streamlit_app/helpers.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 
 class FastAPIClient:
-    BASE_URL = os.getenv("PRODUCTION_URL", "http://127.0.0.1:8000")
+    BASE_URL = st.secrets.get("PRODUCTION_URL", "http://127.0.0.1:8000")
 
     @staticmethod
     def fetch_all_data(sheet_name):
